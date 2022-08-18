8525858b95d044e0e68f4ed9e0693e9540461cbd
laurasierra17 <laura.sierra17@gmail.com>
2022-08-18 00:14:50 -0400

    get method to format timestamps on query

--- models/Reaction.js
+++ models/Reaction.js
@@ -17,7 +17,8 @@ const reactionSchema = new Schema(
         },
         createdAt: {
             type: Date,
-            default: Date.now
+            default: Date.now,
+            get: (timestamp) => new Date(timestamp).toLocaleString()
         }
     },
     {
--- models/Thought.js
+++ models/Thought.js
@@ -12,6 +12,7 @@ const thoughtSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now,
+            get: (timestamp) => new Date(timestamp).toLocaleString()
         },
         username: {
             type: String,
