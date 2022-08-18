8144851d9cb7dcffc2882f987181c8edc6c7fc73
laurasierra17 <laura.sierra17@gmail.com>
2022-08-18 01:04:32 -0400

    data to seed database created

--- models/User.js
+++ models/User.js
@@ -14,7 +14,6 @@ const userSchema = new Schema(
             required: true,
             trim: true,
             lowercase: true,
-            validate: [validateEmail, 'Please fill a valid email address'],
             match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
         },
         thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
---
+++ utils/data.js
@@ -0,0 +1,72 @@
+const usernames = [
+  'zuko123',
+  'zuckerberggg',
+  'theflyingtesla',
+  'sanders4prez',
+  'kanye',
+  'obamaB'
+];
+
+const thoughtBodies = [
+  'How to disagree with someone',
+  'iPhone review',
+  'how-to video',
+  'video essay on the history of video games',
+  'How to make money on the App Store',
+  'Learn NextJS in five minutes (Not clickbate)',
+  'Movie trailer',
+  'Hello world',
+  'Another possible solution to the algorithm',
+  'Apology video',
+  'Submission for startup pitch',
+];
+
+const possibleReactions = [
+  'I disagree!',
+  'I tried your algorithm, here were the results',
+  'This was awesome',
+  'Thank you for the great content',
+  'Please check out my video response',
+  'Like and subscribe to my channel please',
+  'Reply: The side effects of in app purchases on digital marketplaces',
+];
+
+
+// Get a random item given an array
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// Get random username
+const getRandomUsername = () => {
+  return getRandomArrItem(usernames);
+}
+
+// Function to generate random thoughts that we can add to the database. Includes thoughts reactions.
+const getRandomThoughts = (int) => {
+  let results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      thoughtText: getRandomArrItem(thoughtBodies),
+      username: getRandomArrItem(usernames),
+      reactions: [...getThoughtReactions(3)],
+    });
+  }
+  return results;
+};
+
+// Create the responses that will be added to each video
+const getThoughtReactions = (int) => {
+  if (int === 1) {
+    return getRandomArrItem(possibleReactions);
+  }
+  let results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      reactionBody: getRandomArrItem(possibleReactions),
+      username: getRandomArrItem(usernames),
+    });
+  }
+  return results;
+};
+
+// Export the functions for use in seed.js
+module.exports = { usernames, getRandomThoughts, getThoughtReactions };
