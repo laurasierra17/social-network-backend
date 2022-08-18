39e34d173d42531a2145aa8a3fa8a9150bd3bbfc
laurasierra17 <laura.sierra17@gmail.com>
2022-08-18 19:19:11 -0400

    user routes

---
+++ controllers/userController.js
@@ -0,0 +1,52 @@
+const Thought = require('../models/Thought');
+const User = require('../models/User');
+
+module.exports = {
+    getUsers(req, res) {
+        User.find()
+            .populate('thoughts')
+            .populate('friends')
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .populate('thoughts')
+            .populate('friends')
+            .then((user) =>
+                !user
+                    ? res.status(400).json({ message: 'No user with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with this id!' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that id! ' })
+                    : Thought.deleteMany({ _id: { $in: user.thoughts } })
+            )
+            .then(() => res.json({ message: 'User and thoughts deleted!' }))
+            .catch((err) => res.status(500).json(err));
+    }
+}
