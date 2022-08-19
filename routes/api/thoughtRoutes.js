ef76e1fc25106209e5fbf87a0318cd5fab187ecf
laurasierra17 <laura.sierra17@gmail.com>
2022-08-19 01:13:41 -0400

    comments for the thought/reaction routers

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 const { getThoughts, getSingleThought, createThought, updateThought, deleteThought, createReaction, deleteReaction } = require('../../controllers/thoughtController');
 
+// Handles getting the list of thoughts and creating a new thought
 router.route('/').get(getThoughts).post(createThought);
 
+// Handles getting, updating, and deleting a thought based on the idea in the parameters
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
+// Create a new reaction for a specific thought
 router.route('/:thoughtId/reactions').post(createReaction);
 
+// Delete a reaction associated to a thought
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
