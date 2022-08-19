b892a04163f0a08439701da739a2b77f702ed0fa
laurasierra17 <laura.sierra17@gmail.com>
2022-08-19 01:16:29 -0400

    comments for the user routers

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const { getUsers, getSingleUser, createUser, updateUser, deleteUser, addFriend, removeFriend } = require('../../controllers/userController');
 
+// Returns a list of all user and creates a new user
 router.route('/').get(getUsers).post(createUser);
 
+// Gets, updates, and deletes a single user based on their id
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
+// Handles the creation and deletion of a friend associated with a user
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
