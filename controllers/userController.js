28309df11b2ff0c9bac758bc13a1d4b1f698abbb
laurasierra17 <laura.sierra17@gmail.com>
2022-08-19 01:15:16 -0400

    comments for the user controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,13 @@ const Thought = require('../models/Thought');
 const User = require('../models/User');
 
 module.exports = {
+    // Get a list of all users
     getUsers(req, res) {
         User.find()
             .then((users) => res.json(users))
             .catch((err) => res.status(500).json(err));
     },
+    // Returns a single user based on the user id passed through the parameters
     getSingleUser(req, res) {
         User.findOne({ _id: req.params.userId })
             .select('-__v')
@@ -19,11 +21,13 @@ module.exports = {
             )
             .catch((err) => res.status(500).json(err));
     },
+    // Creates a new user based on input sent through the request body
     createUser(req, res) {
         User.create(req.body)
             .then((user) => res.json(user))
             .catch((err) => res.status(500).json(err));
     },
+    // Updates a user based on the id and data sent through the request body
     updateUser(req, res) {
         User.findOneAndUpdate(
             { _id: req.params.userId },
@@ -37,6 +41,7 @@ module.exports = {
             )
             .catch((err) => res.status(500).json(err));
     },
+    // Deletes a user based on its id
     deleteUser(req, res) {
         User.findOneAndDelete({ _id: req.params.userId })
             .then((user) =>
@@ -47,6 +52,7 @@ module.exports = {
             .then(() => res.json({ message: 'User and thoughts deleted!' }))
             .catch((err) => res.status(500).json(err));
     },
+    // Adds a friend to a user
     addFriend(req, res) {
         User.findOneAndUpdate(
             { _id: req.params.userId },
@@ -60,6 +66,7 @@ module.exports = {
             )
             .catch((err) => res.status(500).json(err));
     },
+    // Removes a friend from a user
     removeFriend(req, res) {
         User.findOneAndUpdate(
             { _id: req.params.userId },
